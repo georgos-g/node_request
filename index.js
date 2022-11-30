c6fa80b0d9ce9b8a25f115f9eedb8484a87be91c
georgos-g <gg@dancingbear.de>
2022-11-30 12:17:05 +0100

    Idealo api done

--- index.js
+++ index.js
@@ -3,25 +3,50 @@ import { response } from 'express';
 import { google } from 'googleapis';
 import fetch from 'node-fetch';
 import 'dotenv/config'; // loads env variables from .env file
+import cron from 'node-cron';
 
 const app = express();
 
-const { PORT, IDEALO_CLIENT_ID, IDEALO_CLIENT_SECRET, GOOGLE_SPREADSHEET_ID } =
-  process.env;
+const {
+  PORT,
+  IDEALO_CLIENT_ID,
+  IDEALO_CLIENT_SECRET,
+  GOOGLE_SPREADSHEET_ID,
+  AWIN_ADVERTISER_ID,
+  AWIN_OAUTH_TOKEN,
+  AWIN_OAUTH2_TOKEN,
+} = process.env;
 
 // define port for server
 const port = process.env.PORT || 1553;
 
 app.get('/', async (req, res) => {
-  // Idealo API access token
-  const idealo_data = await generateAccessTokenFetch();
-  // res.json(idealo_data);
-  console.log('IDEALO:::', idealo_data.access_token);
+  // ==================== Idealo API =====================
+  // run cron job every 15 seconds
+  // cron.schedule('*/30 * * * * *', async () => {
+  // cron.schedule('*/1 * * * *', async () => {
+  console.log('running a task every minute');
 
-  const idealo_click_data = await startClickReport();
-  console.log('idealo_click_data:::::: ', idealo_click_data);
+  const idealo_download_click_report = await downloadClickReport();
+  const CSVToArray = (data, delimiter = ',', omitFirstRow = false) =>
+    data
+      .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+      .split('\n')
+      .map((v) => v.split(delimiter));
 
-  //  ======================  Google Sheets API  ===============================
+  const result = CSVToArray(idealo_download_click_report, ';', true);
+  // remove last 2 arrays from result array
+  const idealo_click_result = result.slice(0, 2).reverse();
+  // if date exist in array convert them tp dd.mm.yyyy format
+  idealo_click_result.forEach((element) => {
+    if (element[0]) {
+      element[0] = new Date(element[0]).toLocaleDateString('de-DE');
+    }
+  });
+
+  console.log('result: ', idealo_click_result);
+
+  //  ======================  Google Sheets API  ==========================
 
   const auth = new google.auth.GoogleAuth({
     keyFile: 'google-credentials.json',
@@ -45,25 +70,29 @@ app.get('/', async (req, res) => {
   const getRows = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
-    range: 'Sheet1',
+    range: 'Idealo',
   });
 
-  // Append rows to spreadsheet
+  // Append data to first row of the google spreadsheet
+
   await googleSheets.spreadsheets.values.append({
+    // insert data before the second row of the google spreadsheet
     auth,
     spreadsheetId,
-    range: 'Sheet1!A:B:C',
+    range: 'Idealo!A:Z',
+
     valueInputOption: 'USER_ENTERED',
     resource: {
-      values: [
-        ['Amalia', '25.12.10', 'Berlin'],
-        ['Paloma ', '08.07.15', 'Hamburg'],
-        ['Elena  ', '08.07.77', 'München'],
-      ],
+      // values: idealo_click_result,
+      // [
+      // ['Luisa  ', '08.07.77', 'München'],
+      // ],
     },
   });
   res.send(getRows.data);
+  // });
 });
+
 //  ======================  Idealo API  ===============================
 
 // get idealo access token
@@ -81,32 +110,69 @@ async function generateAccessTokenFetch() {
       },
     }
   );
-  const idealo_data = await response.json();
-  return idealo_data;
+  return await response.json();
 }
 
-// start idealo click report
-async function startClickReport() {
+// // create idealo click report
+// async function startClickReport() {
+//   const idealo_data = await generateAccessTokenFetch();
+//   // const today = new Date().toISOString().slice(0, 10);
+//   const today = new Date(new Date().setDate(new Date().getDate() - 2))
+//     .toISOString()
+//     .slice(0, 10);
+//   console.log('today: ', today);
+//   const yesterday = new Date(new Date().setDate(new Date().getDate() - 3))
+//     .toISOString()
+//     .slice(0, 10);
+//   console.log('yesterday: ', yesterday);
+
+//   const response = await fetch(
+//     `https://businessapi.idealo.com/api/v1/shops/${idealo_data.shop_id}/click-reports`,
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${idealo_data.access_token}`,
+//       },
+//       body: JSON.stringify({
+//         from: yesterday,
+//         to: today,
+//       }),
+//     }
+//   );
+//   return await response.json();
+// }
+
+// download click report
+async function downloadClickReport() {
   const idealo_data = await generateAccessTokenFetch();
-  console.log('idealo_data____: ', idealo_data.access_token);
+  // const idealo_click_report = await startClickReport();
+  // console.log('idealo_click_report.id: ', idealo_click_report.id);
+
+  const date_to = new Date(new Date().setDate(new Date().getDate() - 1))
+    .toISOString()
+    .slice(0, 10);
+
+  // console.log('today: ', today);
+  const date_from = new Date(new Date().setDate(new Date().getDate() - 2))
+    .toISOString()
+    .slice(0, 10);
+
+  console.log('date_from: ', date_from);
 
   const response = await fetch(
-    'https://businessapi.idealo.com/api/v1/shops/12345/daily-click-reports/download?from=2020-09-01&to=2020-09-02',
+    // `https://businessapi.idealo.com/api/v1/shops/${idealo_data.shop_id}/daily-click-reports/${idealo_click_report.id}/download`,
+    `https://businessapi.idealo.com/api/v1/shops/${idealo_data.shop_id}/daily-click-reports/download?from=${date_from}&to=${date_to}`,
     {
+      method: 'GET',
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: `'Bearer ${idealo_data.access_token}'`,
+        'Content-Type': 'application/csv',
+        Authorization: `Bearer ${idealo_data.access_token}`,
       },
-      // body: JSON.stringify({
-      //   from: '2020-09-19',
-      //   to: '2020-09-20',
-      // }),
     }
   );
-
-  // const idealo_click_data = await response.json();
-  const idealo_click_data = await response;
-  return idealo_click_data;
+  // download csv file
+  return await response.text();
 }
 
 app.listen(port, () => console.log('App listening on port ' + port));
