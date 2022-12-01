e3cd81bd81bf56ed7df3bf5744ab99224741fbe0
georgos-g <gg@dancingbear.de>
2022-12-01 08:25:07 +0100

    added awin fetch

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ const {
 const port = process.env.PORT || 1553;
 
 app.get('/', async (req, res) => {
-  // ==================== Idealo API =====================
   // run cron job every 15 seconds
   // cron.schedule('*/30 * * * * *', async () => {
   // cron.schedule('*/1 * * * *', async () => {
   console.log('running a task every minute');
 
+  // ================ Idealo click report =================
   const idealo_download_click_report = await downloadClickReport();
+  // convert csv data to an array
   const CSVToArray = (data, delimiter = ',', omitFirstRow = false) =>
     data
       .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
@@ -36,15 +37,52 @@ app.get('/', async (req, res) => {
 
   const result = CSVToArray(idealo_download_click_report, ';', true);
   // remove last 2 arrays from result array
-  const idealo_click_result = result.slice(0, 2).reverse();
+  const idealo_click_result = result.slice(0, 2);
   // if date exist in array convert them tp dd.mm.yyyy format
   idealo_click_result.forEach((element) => {
     if (element[0]) {
       element[0] = new Date(element[0]).toLocaleDateString('de-DE');
     }
   });
-
   console.log('result: ', idealo_click_result);
+
+  // ================ Awin click report =================
+  const awin_click_report = await getAwinClickReport();
+  // show only the id and date of the awin click report
+  const awin_click_result = awin_click_report.map((item) => {
+    return [
+      item.transactionDate.slice(0, 10),
+
+      item.transactionDate,
+      // item.saleAmount.amount + ' ' + item.saleAmount.currency,
+      item.saleAmount.amount,
+      // item.commissionAmount.amount + ' ' + item.commissionAmount.currency,
+      item.commissionAmount.amount,
+    ];
+  });
+
+  // sum of all item.saleAmount.amount and item.commissionAmount.amount values sorted by date
+  const awin_click_result_sum = awin_click_result.reduce((acc, item) => {
+    if (!acc[item[0]]) {
+      acc[item[0]] = {
+        date: item[0],
+        saleAmount: 0,
+        commissionAmount: 0,
+      };
+    }
+    acc[item[0]].saleAmount += item[2];
+    acc[item[0]].commissionAmount += item[3];
+    return acc;
+  }, {});
+  const awin_click_result_sum_object = Object.values(awin_click_result_sum);
+  // convert awin_click_result_sum_array to array
+  const awin_click_result_sum_array = awin_click_result_sum_object.map(
+    (item) => {
+      return [item.date, item.saleAmount, item.commissionAmount];
+    }
+  );
+
+  console.log('awin_click_result_sum_array: ', awin_click_result_sum_array);
 
   //  ======================  Google Sheets API  ==========================
 
@@ -66,36 +104,76 @@ app.get('/', async (req, res) => {
     spreadsheetId,
   });
 
-  // Read rows from spreadsheet
+  // Read rows from Idealo spreadsheet
   const getRows = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
     range: 'Idealo',
   });
 
-  // Append data to first row of the google spreadsheet
+  // Read rows from Awin spreadsheet
+  const getAwinRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: 'Awin',
+  });
 
+  // Append data to first row of the google spreadsheet
   await googleSheets.spreadsheets.values.append({
-    // insert data before the second row of the google spreadsheet
     auth,
     spreadsheetId,
     range: 'Idealo!A:Z',
 
     valueInputOption: 'USER_ENTERED',
     resource: {
-      // values: idealo_click_result,
-      // [
-      // ['Luisa  ', '08.07.77', 'München'],
-      // ],
+      values: idealo_click_result,
     },
   });
-  res.send(getRows.data);
+
+  // write awin click report to google spreadsheet
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: 'Awin!A:Z',
+
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: awin_click_result_sum_array,
+    },
+  });
+  // get all rows from awin and idealo google spreadsheets
+  const idealo_rows = getRows.data.values;
+  const awin_rows = getAwinRows.data.values;
+  // res.send(getRows.data);
+  res.send({ idealo_rows, awin_rows });
+
   // });
 });
 
+//  ======================  Awin API  ===============================
+async function getAwinClickReport() {
+  const date_to = new Date(new Date().setDate(new Date().getDate() - 1))
+    .toISOString()
+    .slice(0, 10);
+
+  const date_from = new Date(new Date().setDate(new Date().getDate() - 5))
+    .toISOString()
+    .slice(0, 10);
+  const response = await fetch(
+    `https://api.awin.com/advertisers/${AWIN_ADVERTISER_ID}/transactions/?startDate=${date_from}T00%3A00%3A00&endDate=${date_to}T00%3A00%3A00&timezone=UTC`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${AWIN_OAUTH2_TOKEN}`,
+      },
+    }
+  );
+  return await response.json();
+}
 //  ======================  Idealo API  ===============================
 
-// get idealo access token
+// create idealo access token
 async function generateAccessTokenFetch() {
   const response = await fetch(
     'https://businessapi.idealo.com/api/v1/oauth/token',
@@ -143,17 +221,14 @@ async function generateAccessTokenFetch() {
 //   return await response.json();
 // }
 
-// download click report
+// download idealo click report
 async function downloadClickReport() {
   const idealo_data = await generateAccessTokenFetch();
-  // const idealo_click_report = await startClickReport();
-  // console.log('idealo_click_report.id: ', idealo_click_report.id);
 
   const date_to = new Date(new Date().setDate(new Date().getDate() - 1))
     .toISOString()
     .slice(0, 10);
 
-  // console.log('today: ', today);
   const date_from = new Date(new Date().setDate(new Date().getDate() - 2))
     .toISOString()
     .slice(0, 10);
@@ -161,7 +236,6 @@ async function downloadClickReport() {
   console.log('date_from: ', date_from);
 
   const response = await fetch(
-    // `https://businessapi.idealo.com/api/v1/shops/${idealo_data.shop_id}/daily-click-reports/${idealo_click_report.id}/download`,
     `https://businessapi.idealo.com/api/v1/shops/${idealo_data.shop_id}/daily-click-reports/download?from=${date_from}&to=${date_to}`,
     {
       method: 'GET',
