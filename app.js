54e49d9675f819913c84996fc8509f81f4b65e57
Georgos Gakis <georgos_gakis@intention.de>
2023-03-03 13:05:44 +0100

    changed cron job to setInterval

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ import fetch from 'node-fetch';
 import 'dotenv/config'; // loads env variables from .env file
 import cron from 'node-cron';
 
-// // AWS Secret Manager
-// import {
-//   SecretsManagerClient,
-//   GetSecretValueCommand,
-// } from '@aws-sdk/client-secrets-manager';
-
-// const secret_name = 'node_request';
-
-// const client = new SecretsManagerClient({
-//   region: 'eu-central-1',
-// });
-
 // if is development use env variables from .env file and make them global
 if (process.env.NODE_ENV === 'development') {
   const {
@@ -39,197 +27,201 @@ if (process.env.NODE_ENV === 'development') {
 
 const app = express();
 // define port for server
-const port = process.env.PORT || 1553;
+const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
-  // run cron job every 24 hours
-  // cron.schedule('0 0 * * *', async () => {
-  cron.schedule('*/30 * * * * *', async () => {
-    // cron.schedule('*/1 * * * *', async () => {
-    console.log('running a task every minute');
+// ======= Fn to fetch data from Idealo and Awin API ========
+const handleGetRequest = async (req, res) => {
+  // ================ Idealo click report =================
+  const idealo_download_click_report = await downloadClickReport();
+  // convert csv data to an array
+  const CSVToArray = (data, delimiter = ',', omitFirstRow = false) =>
+    data
+      .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
+      .split('\n')
+      .map((v) => v.split(delimiter));
 
-    // ================ Idealo click report =================
-    const idealo_download_click_report = await downloadClickReport();
-    // convert csv data to an array
-    const CSVToArray = (data, delimiter = ',', omitFirstRow = false) =>
-      data
-        .slice(omitFirstRow ? data.indexOf('\n') + 1 : 0)
-        .split('\n')
-        .map((v) => v.split(delimiter));
+  const result = CSVToArray(idealo_download_click_report, ';', true);
+  // remove last 2 (not necessary) arrays from result array
+  const idealo_click_result = result.slice(0, 2);
+  // if date exist in array convert them tp dd.mm.yyyy format
+  idealo_click_result.forEach((element) => {
+    if (element[0]) {
+      element[0] = new Date(element[0]).toLocaleDateString('de-DE');
+    }
+  });
+  // if cost exist format data
+  idealo_click_result.forEach((element) => {
+    if (element[2]) {
+      // remove EUR from cost
+      element[2] = element[2].replace('EUR', '');
+      // convert point to comma from cost
+      element[2] = element[2].replace('.', ',');
+      // remove whitespace from cost
+      element[2] = element[2].trim();
+    }
+  });
 
-    const result = CSVToArray(idealo_download_click_report, ';', true);
-    // remove last 2 (not necessary) arrays from result array
-    const idealo_click_result = result.slice(0, 2);
-    // if date exist in array convert them tp dd.mm.yyyy format
-    idealo_click_result.forEach((element) => {
-      if (element[0]) {
-        element[0] = new Date(element[0]).toLocaleDateString('de-DE');
-      }
-    });
-    // if cost exist format data
-    idealo_click_result.forEach((element) => {
-      if (element[2]) {
-        // remove EUR from cost
-        element[2] = element[2].replace('EUR', '');
-        // convert point to comma from cost
-        element[2] = element[2].replace('.', ',');
-        // remove whitespace from cost
-        element[2] = element[2].trim();
-      }
-    });
-
-    // ================ Awin click report =================
-    const awin_click_report = await getAwinClickReport();
-    const awin_click_result = awin_click_report.map(
-      ({
+  // ================ Awin click report =================
+  const awin_click_report = await getAwinClickReport();
+  const awin_click_result = awin_click_report.map(
+    ({
+      transactionDate,
+      saleAmount: { amount: saleAmount },
+      commissionAmount: { amount: commissionAmount },
+    }) => {
+      return [
+        transactionDate.slice(0, 10),
         transactionDate,
-        saleAmount: { amount: saleAmount },
-        commissionAmount: { amount: commissionAmount },
-      }) => {
-        return [
-          transactionDate.slice(0, 10),
-          transactionDate,
-          saleAmount,
-          commissionAmount,
-        ];
-      }
-    );
-    // sum of all item.saleAmount.amount and item.commissionAmount.amount values sorted by date
-    const awin_click_result_sum = awin_click_result.reduce((acc, item) => {
-      if (!acc[item[0]]) {
-        acc[item[0]] = {
-          date: item[0],
-          saleAmount: 0,
-          commissionAmount: 0,
-        };
-      }
-      acc[item[0]].saleAmount += item[2];
-      acc[item[0]].commissionAmount += item[3];
-      return acc;
-    }, {});
+        saleAmount,
+        commissionAmount,
+      ];
+    }
+  );
+  // sum of all item.saleAmount.amount and item.commissionAmount.amount values sorted by date
+  const awin_click_result_sum = awin_click_result.reduce((acc, item) => {
+    if (!acc[item[0]]) {
+      acc[item[0]] = {
+        date: item[0],
+        saleAmount: 0,
+        commissionAmount: 0,
+      };
+    }
+    acc[item[0]].saleAmount += item[2];
+    acc[item[0]].commissionAmount += item[3];
+    return acc;
+  }, {});
 
-    const awin_click_result_sum_object = Object.values(awin_click_result_sum);
-    // convert awin_click_result_sum_array to array sorted by date
-    const awin_click_result_sum_array = awin_click_result_sum_object.map(
-      (item) => {
-        return [item.date, item.saleAmount, item.commissionAmount];
-      }
-    );
+  const awin_click_result_sum_object = Object.values(awin_click_result_sum);
+  // convert awin_click_result_sum_array to array sorted by date
+  const awin_click_result_sum_array = awin_click_result_sum_object.map(
+    (item) => {
+      return [item.date, item.saleAmount, item.commissionAmount];
+    }
+  );
 
-    // if date exist in array convert them tp dd.mm.yyyy format
-    awin_click_result_sum_array.forEach((element) => {
-      if (element[0]) {
-        element[0] = new Date(element[0]).toLocaleDateString('de-DE');
-      }
-
-      // if saleAmount exist format data
-      if (element[1]) {
-        element[1] = element[1].toFixed(2);
-        element[1] = element[1].replace('.', ',');
-        // add a dot after 3rd number
-        element[1] = element[1].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-      }
-      // if commissionAmount exist format data
-      if (element[2]) {
-        element[2] = element[2].toFixed(2);
-        element[2] = element[2].replace('.', ',');
-        // add a dot after 3rd number
-        element[2] = element[2].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-      }
-    });
-
-    //  ======================  Google Sheets API  ==========================
-
-    const auth = new google.auth.GoogleAuth({
-      keyFile: 'google-credentials.json',
-      scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    });
-
-    // Create client instance for auth
-    const client = await auth.getClient();
-
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: 'v4', auth: client });
-    const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
-
-    // Get metadata about spreadsheets
-    const metaData = await googleSheets.spreadsheets.get({
-      auth,
-      spreadsheetId,
-    });
-
-    // Read rows from Idealo spreadsheet
-    const getIdealoRows = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      // range: 'Idealo!A2:C',
-      range: 'Idealo',
-    });
-
-    // Read rows from Awin spreadsheet
-    const getAwinRows = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: 'Awin',
-    });
-
-    // convert last array of getIdealoRows.data and last array of idealo_click_result to json and check if the are equal
-    const [lastRow] = getIdealoRows.data.values.slice(-1);
-    const lastRowJson = JSON.stringify(lastRow);
-    const idealoLastRowJson = JSON.stringify(idealo_click_result.slice(-2)[0]);
-
-    if (lastRowJson === idealoLastRowJson) {
-      console.log('Idealo click result is up to date');
-    } else {
-      console.log('New data in idealo click result');
-
-      // Update Idealo spreadsheet
-      await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: 'Idealo!A:Z',
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-          values: idealo_click_result,
-        },
-      });
+  // if date exist in array convert them tp dd.mm.yyyy format
+  awin_click_result_sum_array.forEach((element) => {
+    if (element[0]) {
+      element[0] = new Date(element[0]).toLocaleDateString('de-DE');
     }
 
-    // convert last array of getAwinRows.data to json
-    const getAwinRows_last_array_json = JSON.stringify(
-      getAwinRows.data.values[getAwinRows.data.values.length - 1]
-    );
-
-    // convert last array of awin_click_result_sum_array to json
-    const awin_click_result_sum_array_last_array = JSON.stringify(
-      awin_click_result_sum_array[awin_click_result_sum_array.length - 1]
-    );
-
-    if (
-      getAwinRows_last_array_json !== awin_click_result_sum_array_last_array
-    ) {
-      // write awin click report to google spreadsheet
-      await googleSheets.spreadsheets.values.append({
-        auth,
-        spreadsheetId,
-        range: 'Awin!A:Z',
-
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-          values: awin_click_result_sum_array,
-        },
-      });
-    } else {
-      console.log('awin_click_result_sum is up to date');
+    // if saleAmount exist format data
+    if (element[1]) {
+      element[1] = element[1].toFixed(2);
+      element[1] = element[1].replace('.', ',');
+      // add a dot after 3rd number
+      element[1] = element[1].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
     }
+    // if commissionAmount exist format data
+    if (element[2]) {
+      element[2] = element[2].toFixed(2);
+      element[2] = element[2].replace('.', ',');
+      // add a dot after 3rd number
+      element[2] = element[2].replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    }
+  });
 
-    // get all rows from awin and idealo google spreadsheets
-    const idealo_rows = getIdealoRows.data.values;
-    const awin_rows = getAwinRows.data.values;
-    // res.send(getRows.data);
-    res.send({ idealo_rows, awin_rows });
-  }); //cron job
-});
+  //  ======================  Google Sheets API  ==========================
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'google-credentials.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: 'v4', auth: client });
+  const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
+
+  // Get metadata about spreadsheets
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from Idealo spreadsheet
+  const getIdealoRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    // range: 'Idealo!A2:C',
+    range: 'Idealo',
+  });
+
+  // Read rows from Awin spreadsheet
+  const getAwinRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: 'Awin',
+  });
+
+  // convert last array of getIdealoRows.data and last array of idealo_click_result to json and check if the are equal
+  const [lastRow] = getIdealoRows.data.values.slice(-1);
+  const lastRowJson = JSON.stringify(lastRow);
+  const idealoLastRowJson = JSON.stringify(idealo_click_result.slice(-2)[0]);
+
+  if (lastRowJson === idealoLastRowJson) {
+    console.log('Idealo click result is up to date');
+  } else {
+    console.log('New data in idealo click result');
+
+    // Update Idealo spreadsheet
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: 'Idealo!A:Z',
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: idealo_click_result,
+      },
+    });
+  }
+
+  // convert last array of getAwinRows.data to json
+  const getAwinRows_last_array_json = JSON.stringify(
+    getAwinRows.data.values[getAwinRows.data.values.length - 1]
+  );
+
+  // convert last array of awin_click_result_sum_array to json
+  const awin_click_result_sum_array_last_array = JSON.stringify(
+    awin_click_result_sum_array[awin_click_result_sum_array.length - 1]
+  );
+
+  if (getAwinRows_last_array_json !== awin_click_result_sum_array_last_array) {
+    // write awin click report to google spreadsheet
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: 'Awin!A:Z',
+
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: awin_click_result_sum_array,
+      },
+    });
+  } else {
+    console.log('awin_click_result_sum is up to date');
+  }
+
+  // get all rows from awin and idealo google spreadsheets
+  const idealo_rows = getIdealoRows.data.values;
+  const awin_rows = getAwinRows.data.values;
+  // res.send(getRows.data);
+  res.send({ idealo_rows, awin_rows });
+};
+
+// Run the initial app.get request at startup
+app.get('/', handleGetRequest);
+
+// Run the app.get request once every 24 hours
+
+// const intervalInMs = 15 * 1000; // 15 seconds  for testing
+const intervalInMs = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+setInterval(() => {
+  app.get('/', handleGetRequest);
+  console.log('Requested Data once in 24 Hours');
+}, intervalInMs);
 
 //  ======================  Awin API  ===============================
 async function getAwinClickReport() {
